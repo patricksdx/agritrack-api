54f00d6565aec70ad331bc731f224014927fc00c
Patrick <patrick74966586@gmail.com>
2025-05-30 22:51:11 -0500

    Actualización del archivo .env para incluir la variable HOST. Se añaden nuevas dependencias de compresión en package.json. Refactorización de server.ts para integrar el middleware de compresión y simplificar la configuración del servidor, eliminando código relacionado con la autenticación de sockets.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
-import { Server, Socket } from "socket.io";
-import jwt from "jsonwebtoken";
+import { Server } from "socket.io";
 import { createServer } from "http";
 import cookieParser from "cookie-parser";
+import compression from "compression";
 
 import { connectDB, sequelize } from "./config/db";
 import { corsOptions } from "./config/utils/cors";
@@ -12,6 +12,7 @@ import authUsuarioRouter from "./routes/usuario/AuthUsuarioRoutes";
 import plantasRouter from "./routes/planta/PlantasRoutes";
 
 const app = express();
+app.use(compression());
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
@@ -22,64 +23,24 @@ const io = new Server(httpServer, {
   cors: corsOptions,
 });
 
+app.use("/api/usuario/auth", authUsuarioRouter);
+app.use("/api/plantas", plantasRouter);
+
+io.on("connection", (socket) => {
+  console.log("🔌 Socket conectado:", socket.id);
+});
+
 (async () => {
   try {
     await connectDB();
     await sequelize.sync({ alter: true });
     console.log("📜 Base de datos lista.");
 
-    const SECRET_KEY = process.env.SECRET_KEY;
-    if (!SECRET_KEY) {
-      throw new Error("Falta la clave de seguridad.");
-    }
+    const PORT = process.env.PORT || 3000;
+    const HOST = process.env.HOST || "http://192.168.0.35";
 
-    interface CustomSocket extends Socket {
-      user?: { usuario_id: number; usuario_email: string };
-    }
-
-    io.use((socket: CustomSocket, next) => {
-      const cookies = socket.handshake.headers.cookie;
-
-      if (cookies) {
-        const tokenMatch = cookies.match(/authToken=([^;]*)/);
-        const token = tokenMatch ? tokenMatch[1] : null;
-
-        if (token) {
-          try {
-            const decoded = jwt.verify(token, SECRET_KEY) as {
-              usuario_id: number;
-              usuario_email: string;
-            };
-            socket.user = decoded;
-            console.log("✅ Usuario autenticado:", socket.user);
-          } catch (error) {
-            console.error("❌ Token inválido:", error);
-          }
-        }
-      }
-
-      next();
-    });
-
-    app.use("/api/usuario", authUsuarioRouter);
-    app.use("/api/plantas", plantasRouter);
-
-    io.on("connection", (socket: CustomSocket) => {
-      console.log("🔌 Socket conectado:", socket.user?.usuario_email);
-
-      if (socket.user) {
-        console.log("🔓 Usuario autenticado, acceso permitido:", socket.user);
-      } else {
-        console.log(
-          "⚠️ Usuario sin autenticación, solo login y register permitido."
-        );
-      }
-    });
-
-    httpServer.listen(process.env.PORT, () => {
-      console.log(
-        `🚀 Servidor escuchando en http://192.168.0.5:${process.env.PORT}`
-      );
+    httpServer.listen(PORT, () => {
+      console.log(`🚀 Servidor escuchando en ${HOST}:${PORT}`);
     });
   } catch (error) {
     console.error("❌ Error al conectar la base de datos:", error);
